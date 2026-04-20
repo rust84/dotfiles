cc7ffc845f4388ce7db5d25143eb84954b5bf13c
Russell Hall <owner@russellhall.co.uk>
2026-04-20 10:52:49 +0100

    fix: use AQUA_ROOT_DIR consistently in fish config
    Editor config hardcoded the old ~/.local/share/aqua/bin path, so
    aliases silently dropped after AQUA_ROOT_DIR moved to aquaproj-aqua.
    Also collapse the duplicate fish_add_path now that both entries
    resolve to the same directory.

--- private_dot_config/private_fish/conf.d/editor.fish
+++ private_dot_config/private_fish/conf.d/editor.fish
@@ -1,10 +1,10 @@
-if type -q $HOME/.local/share/aqua/bin/nvim
-    set -gx EDITOR $HOME/.local/share/aqua/bin/nvim
-    set -gx VISUAL $HOME/.local/share/aqua/bin/nvim
-    set -gx KUBE_EDITOR $HOME/.local/share/aqua/bin/nvim
-    alias vim $HOME/.local/share/aqua/bin/nvim
-    alias vi $HOME/.local/share/aqua/bin/nvim
-    alias nano $HOME/.local/share/aqua/bin/nvim
+if type -q $AQUA_ROOT_DIR/bin/nvim
+    set -gx EDITOR $AQUA_ROOT_DIR/bin/nvim
+    set -gx VISUAL $AQUA_ROOT_DIR/bin/nvim
+    set -gx KUBE_EDITOR $AQUA_ROOT_DIR/bin/nvim
+    alias vim $AQUA_ROOT_DIR/bin/nvim
+    alias vi $AQUA_ROOT_DIR/bin/nvim
+    alias nano $AQUA_ROOT_DIR/bin/nvim
 else
     alias nvim vim
 end
